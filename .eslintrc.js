6705460b7715be94f067dd7498a32cea7bc8f63f
楼教主 <cik520@qq.com>
2018-02-04 00:36:38 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: 'airbnb-base',
+  rules: {
+    'no-param-reassign': 'off',
+    'no-restricted-syntax': 'off',
+    'no-await-in-loop': 'off',
+  },
+};
